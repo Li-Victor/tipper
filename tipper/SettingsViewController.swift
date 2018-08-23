7344872fc0a24ea7e2be35f34a088412d793cbd1
Victor Li <victorli5674@yahoo.com>
2018-08-22 21:21:36 -0700

    Settings page to change the default tip percentage

--- tipper/SettingsViewController.swift
+++ tipper/SettingsViewController.swift
@@ -11,36 +11,76 @@ import UIKit
 class SettingsViewController: UIViewController {
 
     @IBOutlet weak var firstTipSlider: UISlider!
-    @IBOutlet weak var firstTipLabel: UILabel!
     @IBOutlet weak var secondTipSlider: UISlider!
-    @IBOutlet weak var secondTipLabel: UILabel!
     @IBOutlet weak var thirdTipSlider: UISlider!
+    
+    @IBOutlet weak var firstTipLabel: UILabel!
+    @IBOutlet weak var secondTipLabel: UILabel!
     @IBOutlet weak var thirdTipLabel: UILabel!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    let defaults = UserDefaults.standard
+    
+    func setText(of label: UILabel, to text: String) {
+        label.text = text
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func setValue(of slider: UISlider, to number: Int) {
+        slider.setValue(Float(number), animated: false)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if defaults.double(forKey: "firstTip") != 0.0 {
+            let tip = Int(defaults.double(forKey: "firstTip"))
+            setText(of: firstTipLabel, to: "\(tip)%")
+            setValue(of: firstTipSlider, to: tip)
+        } else {
+            // default value
+            setText(of: firstTipLabel, to: "18%")
+            setValue(of: firstTipSlider, to: 18)
+        }
+        
+        if defaults.double(forKey: "secondTip") != 0.0 {
+            let tip = Int(defaults.double(forKey: "secondTip"))
+            setText(of: secondTipLabel, to: "\(tip)%")
+            setValue(of: secondTipSlider, to: tip)
+        } else {
+            // default value
+            setText(of: secondTipLabel, to: "20%")
+            setValue(of: secondTipSlider, to: 20)
+        }
+        
+        if defaults.double(forKey: "thirdTip") != 0.0 {
+            let tip = Int(defaults.double(forKey: "thirdTip"))
+            setText(of: thirdTipLabel, to: "\(tip)%")
+            setValue(of: thirdTipSlider, to: tip)
+        } else {
+            // default value
+            setText(of: thirdTipLabel, to: "25%")
+            setValue(of: thirdTipSlider, to: 25)
+        }
     }
     
     @IBAction func firstSliderChanged(_ sender: UISlider) {
         let value = Int(sender.value)
         firstTipLabel.text = "\(value)%"
+        defaults.set(Double(value), forKey: "firstTip")
+        defaults.synchronize()
     }
     
     @IBAction func secondSliderChanged(_ sender: UISlider) {
         let value = Int(sender.value)
         secondTipLabel.text = "\(value)%"
+        defaults.set(Double(value), forKey: "secondTip")
+        defaults.synchronize()
     }
     
     @IBAction func thirdSliderChanged(_ sender: UISlider) {
         let value = Int(sender.value)
         thirdTipLabel.text = "\(value)%"
+        defaults.set(Double(value), forKey: "thirdTip")
+        defaults.synchronize()
     }
     
     /*
--- tipper/ViewController.swift
+++ tipper/ViewController.swift
@@ -14,26 +14,38 @@ class ViewController: UIViewController {
     @IBOutlet weak var billLabel: UITextField!
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+    
+    let defaults = UserDefaults.standard
+    // default tip percentages
+    var tipPercentages = [18.0, 20.0, 25.0]
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if defaults.double(forKey: "firstTip") != 0.0 {
+            tipPercentages[0] = defaults.double(forKey: "firstTip")
+        }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    @IBAction func onTap(_ sender: Any) {
-        view.endEditing(true)
+        if defaults.double(forKey: "secondTip") != 0.0 {
+            tipPercentages[1] = defaults.double(forKey: "secondTip")
+        }
+        
+        if defaults.double(forKey: "thirdTip") != 0.0 {
+            tipPercentages[2] = defaults.double(forKey: "thirdTip")
+        }
+        
+        for i in tipPercentages.indices {
+            let tip = Int(tipPercentages[i])
+            tipControl.setTitle("\(tip)%", forSegmentAt: i)
+        }
+        
+        // recalculate tip if the selected segment control has been updated
+        updateLabels()
     }
     
-    @IBAction func calculateTip(_ sender: Any) {
-        
-        let tipPercentages = [0.18, 0.2, 0.25]
-        
+    func updateLabels() {
         if let bill = Double(billLabel.text!) {
-            let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+            let tip = bill * (tipPercentages[tipControl.selectedSegmentIndex] / 100)
             let total = bill + tip
             tipLabel.text = String(format: "$%.02f", tip)
             totalLabel.text = String(format: "$%.02f", total)
@@ -41,7 +53,14 @@ class ViewController: UIViewController {
             tipLabel.text = "$0.00"
             totalLabel.text = "$0.00"
         }
-        
+    }
+
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        updateLabels()
     }
 }
 
